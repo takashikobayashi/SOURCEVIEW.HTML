16ee3a048fff0ed5fb8ea353f730f5b96f05f0ed
sumo_ninja_jp <tkgrove@gmail.com>
2014-07-03 09:16:23 +0900

    Update SCRIPT.js

--- SCRIPT.js
+++ SCRIPT.js
@@ -1,52 +1,54 @@
-/* Source View | SyntaxHighlirhter | JavaScript, html5 and CSS3 Study | (c) 2014 twitter@sumo_ninja_jp
+/* Source View | SyntaxHighlirhter, jQuery, AngularJS | JavaScript, html5, CSS3 Study | (c) 2014 twitter@sumo_ninja_jp
 */
 
-var rp = function() {
+function SourceViewModel( $scope, $http ) {
   var param = window.location.search.substring( 1 );
   if( param === undefined || param.length <= 0 ) {
-    $( "#sv_title" ).text( "No parameters." );
-    return;
+    $scope.sv_title = "No parameters.";
   }
 
   var obj = JSON.parse( decodeURIComponent( param ) );
-  var xhr;
+  var files=[];
+
   if( obj.entry !== undefined ) {
-    xhr = new XMLHttpRequest();
-    xhr.open( "GET", obj.url + obj.entry, false );
-    xhr.onload = function() {
-      param = xhr.responseText;
-      obj = JSON.parse( param );
-      sv( obj );
-    }
-    xhr.send();
+    $http.get( obj.url + obj.entry ).then( function( res ) {
+      setEntry( res.data );
+    } );
   }
   else {
-    sv( obj );
-  }
-}
-
-var sv = function( obj ) {
-  $( "#sv_title" ).text( obj.title );
-  $( "#sv_url" ).text( obj.url );
-
-  var i = obj.files.length;
-  while( i > 0 ) {
-    $( "#sv_url" ).after( "<pre title='" + obj.files[--i].title + "' />" );
+    setEntry( obj );
   }
 
-  var xhrs = [];
-  for( i = 0; i < obj.files.length; i++ ) {
-    xhrs[i] = new XMLHttpRequest();
-    xhrs[i].open( "GET", obj.url + obj.files[i].name, false );
-    xhrs[i].onload = function() {
-      var pre = "pre[title='" + obj.files[i].title + "']";
-      $( pre ).text( xhrs[i].responseText );
-      $( pre ).addClass( "brush: " + obj.files[i].brush );
+  function setEntry( obj ) {
+    $scope.sv_title = obj.title;
+    $scope.sv_url = obj.url;
+    $scope.sv_files = obj.files;
 
-      SyntaxHighlighter.highlight();
+    if( obj.exec != undefined ) {
+      $scope.sv_exec = obj.url + obj.exec;
     }
-    xhrs[i].send();
-  }
-}
 
-window.addEventListener( "load", rp, false);
+    return setFileContents( 0 );
+  };
+
+  function setFileContents( i ) {
+    if( i < $scope.sv_files.length ) {
+      $http.get( $scope.sv_url + $scope.sv_files[i].name ).then( function( obj ) {
+        files.push( obj.data );
+        return setFileContents( ++i );
+      } );
+    }
+    else {
+      return setHighlight();
+    }
+  };
+
+  function setHighlight() {
+    var i;
+    for( i = 0; i < files.length; i++ ) {
+      angular.element( "pre[title='" + $scope.sv_files[i].title + "']" ).text( files[i] );
+    }
+
+     SyntaxHighlighter.highlight();
+  };
+}
